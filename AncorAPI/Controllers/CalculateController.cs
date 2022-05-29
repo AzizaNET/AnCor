1222af97981ae00fabf1638eed642e27d53bacd1
Aziza Abdurakhmonova <aziza_abdurakhmonova@epam.com>
2022-05-29 15:53:26 +0500

    Calculation added

---
+++ AncorAPI/Controllers/CalculateController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AncorAPI.Models;
+using AncorAPI.Repository;
+
+namespace AncorAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculateController : ControllerBase
+    {
+        private readonly ITransferRepository _transferRepository;
+
+        public CalculateController(ITransferRepository transferRepository)
+        {
+            _transferRepository = transferRepository;
+        }
+        [HttpGet]
+        public async Task<CalculationResult> Get()
+        {
+            decimal salary = 1000000;
+
+            var resCollection = await _transferRepository.Get();
+            var creditAmount = resCollection.Sum(o => o.CreditAmount);
+            var debtAmount = resCollection.Sum(d => d.DebtAmount);
+
+            var amount = creditAmount - debtAmount;
+
+            
+            CalculationResult obj = new CalculationResult();
+            obj.Amount = amount;
+
+            if (amount > salary) 
+            {
+                obj.Status = "yellow";
+                
+            }
+
+            return obj;
+        }
+    }
+}
---
+++ AncorAPI/Models/CalculationResult.cs
@@ -0,0 +1,9 @@
+﻿namespace AncorAPI.Models
+{
+    public class CalculationResult
+    {
+        public decimal? Amount { get; set; }
+        public string Status { get; set; }
+
+    }
+}
